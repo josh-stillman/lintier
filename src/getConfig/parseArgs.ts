3435dd591ac21348b205a40a81b4affc8cf7f0bf
Josh Stillman <josh.stillman@giantmachines.com>
2020-12-09 14:01:28 -0500

    fix cl args casing

--- src/getConfig/parseArgs.ts
+++ src/getConfig/parseArgs.ts
@@ -6,10 +6,10 @@ export const getArgs = () => {
   program
     .option('-r, --react', 'install react dependencies')
     .option('-n, --node', 'install node dependencies')
-    .option('-a, --airbnb', 'install airbnb styleguide')
-    .option('-s, --stylelint', 'install stylelint')
+    .option('-a, --airBnb', 'install airbnb styleguide')
+    .option('-s, --styleLint', 'install stylelint')
     .option('-h, --husky', 'install husky and lint-staged');
-  // version etc.
+  // TODO: version etc.
   program.parse(process.argv);
 
   return program.opts();
