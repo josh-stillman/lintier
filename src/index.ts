0d834a6381c0510805ea9d4704c91f866d779ec3
Josh Stillman <josh.stillman@giantmachines.com>
2021-05-26 16:25:33 -0400

    move todo list to github project

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,6 @@
 
 /* eslint-disable no-console */
 
-// TODO:
-// X 1. parse options and install accordingly
-// X 1. logging about what is being installed (proejct type).  spinner?
-// X 2. edit eslint accordingly
-// X 3. prune unused rules.  (Try out on react projects.  iching)
-// X 8. better repo naming.  ts-prettylint? ts-lintier.... just lintier?
-// X 6. spinners / new lines after logs
-// X 4. stylelint
-// X 6. clean up and organize code (alter config obj)
-// X 7. new lines before spinner output.
-// X 7. help explaining how it's used, version,
-// X 7. readme.
-// X 8. write separate eslint ts.config for project?
-// X 12. setup ci/cd tests and linting in github actions
-
-// 8. styled components - try disabling stylelint/prettier for .ts/x files.
-
-// 9. add hyperlinks to success msg for vs code plugins. https://github.com/sindresorhus/terminal-link
-// 5. husky
-
-// 10. add question about proceeding without git?
-// 9. get rid of console log for options
-
-// 1. Chalk colors?
-
-// 13. setup ci/cd publish to npm on merge to main on github actions
-// 10. Pin versions - one release pinned, one unpinned to catch new updates
-// 7. test on npx.
-
-// future considerations:
-// // 11. use js files for configs.
-// 1. Ink react components?
-
-// move to GH project
-
 import fs from 'fs';
 import path from 'path';
 import { exit } from 'process';
