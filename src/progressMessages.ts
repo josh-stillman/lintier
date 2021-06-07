5771716154fffd6be8cef610f12b92846ebae02e
Josh Stillman <josh.stillman@giantmachines.com>
2021-06-07 14:55:39 -0400

    update logging output using chalk

---
+++ src/progressMessages.ts
@@ -0,0 +1,10 @@
+export const PROGRESS_MESSAGES = {
+  dependencies: 'Installing dependencies...',
+  airbnb: 'Installing airbnb...',
+  prettier: 'Writing .prettierrc...',
+  eslint: 'Writing .eslintrc...',
+  tsconfig: 'Writing eslint tsconfig file...',
+  stylelint: 'Writing .stylelintrc...',
+  lintStaged: 'Writing lint-staged config...',
+  packageJson: 'Updating package.json with lint scripts...',
+};
