af3ff8fa1a578ed1920ab1964e6b827c3e7b0530
Josh Stillman <joshstillman@gmail.com>
2024-09-19 09:13:04 -0400

    fix linting errors from test repo

--- e2eFixtures/test-repo/src/server/index.ts
+++ e2eFixtures/test-repo/src/server/index.ts
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 app.get<{}, { message: string }>('/', (req, res) => {
   res.json({
     message: 'hello world',
@@ -12,6 +13,5 @@ app.get<{}, { message: string }>('/', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
 });
